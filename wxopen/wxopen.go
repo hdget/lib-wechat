807cf3347d92caa594631e192865869dd4bd26bb
Ryan Fan <reg_info@qq.com>
2025-06-16 12:22:23 +0800

    add login to wxopen (#5)

--- wxopen/wxopen.go
+++ wxopen/wxopen.go
@@ -3,6 +3,7 @@ package wxopen
 import "github.com/hdget/lib-wechat/wxopen/api"
 
 type Lib interface {
+	Login(code string) (string, string, error) // 获取OpenId和UnionId
 }
 
 type wxopenImpl struct {
@@ -13,4 +14,8 @@ func New(appId, appSecret string) Lib {
 	return &wxopenImpl{
 		api: api.New(appId, appSecret),
 	}
+}
+
+func (impl wxopenImpl) Login(code string) (string, string, error) {
+	return impl.api.Login(code)
 }
