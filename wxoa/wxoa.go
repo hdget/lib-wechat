e93da9bdd2f338d15f6b2b32222846341269fd47
Ryan Fan <reg_info@qq.com>
2025-06-18 10:41:32 +0800

    change verify server (#6)

--- wxoa/wxoa.go
+++ wxoa/wxoa.go
@@ -7,16 +7,17 @@ import (
 	"github.com/hdget/lib-wechat/wxoa/api"
 	"github.com/hdget/lib-wechat/wxoa/cache"
 	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
 	"io"
 	"sort"
 	"strings"
 )
 
 type Lib interface {
-	CreateJsSdkSignature(url string) (*JsSdkSignatureResult, error) // 生成jsapi_ticket签名
-	VerifyServer(signature, token, timestamp, nonce string) bool    // 校验微信公众号服务器
-	HandleMessage(data []byte) ([]byte, error)                      // 接收普通消息,接收事件消息以及被动回复消息
-	SendTemplateMessage(toUser string, msg *TemplateMessage) error  // 发送模板消息
+	CreateJsSdkSignature(url string) (*JsSdkSignatureResult, error)           // 生成jsapi_ticket签名
+	VerifyServer(token string, queryParams map[string]string) (string, error) // 校验微信公众号服务器
+	HandleMessage(data []byte) ([]byte, error)                                // 接收普通消息,接收事件消息以及被动回复消息
+	SendTemplateMessage(toUser string, msg *TemplateMessage) error            // 发送模板消息
 	//GetUserInfo(openid string) (*api.UserInfoResult, error)         // 获取用户信息
 }
 
@@ -32,16 +33,26 @@ func New(appId, appSecret string, redisProvider intf.RedisProvider) Lib {
 	}
 }
 
-// VerifyServer 公众号接入时校验服务器
+// VerifyServer 公众号接入时校验
 // 参考： https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Access_Overview.html
-func (impl *wxoaImpl) VerifyServer(signature, token, timestamp, nonce string) bool {
+func (impl *wxoaImpl) VerifyServer(token string, queryParams map[string]string) (string, error) {
+	signature, timestamp, nonce, echostr := queryParams["signature"], queryParams["timestamp"], queryParams["nonce"], queryParams["echostr"]
+	if signature == "" || timestamp == "" || nonce == "" {
+		return "", fmt.Errorf("invalid request, urlParams: %v", queryParams)
+	}
+
 	si := []string{token, timestamp, nonce}
 	sort.Strings(si)              //字典序排序
 	str := strings.Join(si, "")   //组合字符串
 	s := sha1.New()               //返回一个新的使用SHA1校验的hash.Hash接口
 	_, _ = io.WriteString(s, str) //WriteString函数将字符串数组str中的内容写入到s中
 	calculatedSignature := fmt.Sprintf("%x", s.Sum(nil))
-	return signature == calculatedSignature
+
+	if signature != calculatedSignature {
+		return "", errors.New("signature not matched")
+	}
+
+	return echostr, nil
 }
 
 func (impl *wxoaImpl) SendTemplateMessage(toUser string, msg *TemplateMessage) error {
